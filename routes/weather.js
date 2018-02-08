215fb84f65e55e3037cc629978d835953a4c893b
Brennan Pilcher <brennan.pilcher@gmail.com>
2018-02-07 21:42:30 -0500

    Initial commit of Node/Express backend to Heroku

---
+++ routes/weather.js
@@ -0,0 +1,37 @@
+var express = require('express');
+var router = express.Router();
+var request = require('request');
+
+/* GET weather by zip code. */
+router.get('/zip/:zip', function(req, res, next) {
+
+  let weatherData = {
+    city : {},
+    currentWeather : {},
+    currentMain : {},
+    currentTime : "",
+  };
+
+  // console.log(req.params.zip);
+
+
+  request('https://api.openweathermap.org/data/2.5/forecast?zip=' + req.params.zip + ',us&units=imperial&APPID=' + process.env.APIKEY, { json: true }, (err, resp, body) => {
+    if (err) { return console.log(err); } // else { console.log(resp) }
+    //console.log(body);
+    //const data = JSON.parse(body);
+    weatherData.currentWeather = body.list[0].weather;
+    weatherData.currentMain = body.list[0].main;
+    weatherData.currentTime = body.list[0].dt_txt;
+    weatherData.city = body.city;
+    res.json(weatherData);
+  });
+
+
+});
+
+
+router.get('/', function(req, res, next) {
+  res.send("invalid request");
+});
+
+module.exports = router;
