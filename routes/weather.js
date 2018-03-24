74106f8cb4289f8e025028f38e0aed9bb30cf6d3
Brennan Pilcher <brennan.pilcher@gmail.com>
2018-03-24 19:33:45 -0400

    Added support for getting weather by lat/long and changed units to return in Kelvin.

--- routes/weather.js
+++ routes/weather.js
@@ -5,27 +5,22 @@ var request = require('request');
 /* GET weather by zip code. */
 router.get('/zip/:zip', function(req, res, next) {
 
-  let weatherData = {
-    city : {},
-    currentWeather : {},
-    currentMain : {},
-    currentTime : "",
-  };
-
-  // console.log(req.params.zip);
-
-  request('https://api.openweathermap.org/data/2.5/forecast?zip=' + req.params.zip + ',us&units=imperial&APPID=' + process.env.APIKEY, { json: true }, (err, resp, body) => {
+  request('https://api.openweathermap.org/data/2.5/forecast?zip=' + req.params.zip + ',us&units=kelvin&APPID=' + process.env.APIKEY, { json: true }, (err, resp, body) => {
     if (err) { return console.log(err); } // else { console.log(resp) }
-    //console.log(body);
-
-    // // Current weather data
-    // weatherData.currentWeather = body.list[0].weather;
-    // weatherData.currentMain = body.list[0].main;
-    // weatherData.currentTime = body.list[0].dt_txt;
-    // weatherData.city = body.city;
 
     res.json(body);
   });
+
+});
+
+/* GET weather by latitude and longitude. */
+router.get('/latlong/:latlong', function(req, res, next) {
+
+  request('https://api.openweathermap.org/data/2.5/forecast?' + req.params.latlong + '&units=kelvin&APPID=' + process.env.APIKEY, { json: true }, (err, resp, body) => {
+    if (err) { return console.log(err); }
+
+    res.json(body);
+  })
 
 });
 
